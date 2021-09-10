76be29a345046ccd44e83b74cd9ee70c4a5c5ea0
sinking <1034439685@qq.com>
2021-09-10 23:45:39 +0800

    第一次上传

---
+++ build.rs
@@ -0,0 +1,4 @@
+fn main() {
+	// .rlib 路径
+    println!("cargo:rustc-link-search=./target/debug/");
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// 导入 test_rlib
+extern crate test_rlib;
+
+fn main() {
+    println!("Hello, world!");
+    // 调用 test 函数
+    test_rlib::test();
+}
